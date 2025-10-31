d1f4a8e12a9f2ad99e603076e7ec05e90f3d96fc
chinky23-tech <chinky0330@gmail.com>
2025-10-31 11:43:00 +0530

    first app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+ function App() {
+  return (
+    <div className="app">
+      <h1>Chinky</h1>
+      <p>I'm learning React and excited to build amazing websites!</p>
+      <h2>My Hobbies:</h2>
+      <ul>
+        <li>Coding</li>
+        <li>Reading</li>
+        <li>Travelling</li>
+      </ul>
+      <button>Learn More</button>
+    </div>
+  );
+}
+export default App;
